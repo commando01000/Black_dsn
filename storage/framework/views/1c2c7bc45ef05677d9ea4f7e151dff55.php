6c7fc74639599bb15f384ae652507898a99a51e5
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-08-16 19:18:56 +0300

    services

--- storage/framework/views/1c2c7bc45ef05677d9ea4f7e151dff55.php
+++ storage/framework/views/1c2c7bc45ef05677d9ea4f7e151dff55.php
@@ -222,7 +222,7 @@
                 data-dsn-option='{"start":"100%","end":"0%","pin":false}'>
 
                 <?php $__currentLoopData = $brands; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $brand): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                    <div class="grid-item" style="width: 400px; height: 100px;">
+                    <div class="grid-item" style="width: 400px; height: 150px;">
                         <div class="image-item" style="width: 100%; height: 100%;">
                             <img class="cover-bg-img" src="<?php echo e(Storage::url($brand->cover)); ?>" alt="<?php echo e($brand->title); ?>"
                                 style="width: 100%; height: 100%; object-fit: contain;">
